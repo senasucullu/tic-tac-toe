f04359918c67140ed2517a83813d29713d0c2a3f
Sena Sücüllü <56732191+senasucullu@users.noreply.github.com>
2022-04-14 15:51:09 +0300

    buton

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { Text , TouchableOpacity} from "react-native";
 import {VStack, HStack, Flex} from "react-native-flex-layout"
 
 
-function Box ({ value, onPress, highlighted }){
+function Box ({ value, onPress,disabled, highlighted }){
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity disabled={disabled} onPress={onPress}>
         <Flex w={74} h={74} center style={{backgroundColor:highlighted ? "lightgreen":"lightgray"}} >
           <Text style={{ fontSize: 56}}>{value}</Text>
         </Flex>
@@ -20,22 +20,39 @@ function App(){
 
   const[board, setBoard] = useState(Array(9).fill(null));
 
+  const[highlighted, setHighlighted] = useState([]);
+
+  const[winner, setWinner] = useState (null);
+
+  const handlePress = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    setCurrentPlayer((prev) => (prev === "X" ? "0" : "X" ));
+
+  };
+
+  const getBox = (index) => (
+    <Box 
+    value={board[index]}
+    onPress={() => handlePress(index)}
+    highlighted={highlighted.includes(index)}
+    disabled={winner || board[index]}
+  />
+  
+  )
+
   return (
     <VStack fill center spacing={2}>
+      <Text style={{ fontSize:36}}>{currentPlayer} to Play </Text>
       <HStack spacing={2} shouldWrapChildren>
-        <Box value="x" highlighted/>
-        <Box />
-        <Box />
+       
       </HStack>   
       <HStack spacing={2} shouldWrapChildren>
-      <Box />
-      <Box />
-      <Box />
+      
       </HStack>   
       <HStack spacing={2} shouldWrapChildren>
-      <Box />
-      <Box />
-      <Box />
+      
       </HStack>
 
     </VStack>
